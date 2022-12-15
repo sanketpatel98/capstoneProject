c0b81c655f46cc955d367c58788ad47252c644ad
sanketpatel <patelsanketr98@gmail.com>
2022-12-14 22:22:25 -0500

    URL changed to aws ec2

--- Frontend/src/backendCalls/backendAddress.js
+++ Frontend/src/backendCalls/backendAddress.js
@@ -1,6 +1,6 @@
-const API_URL =
-  Platform.OS === "ios" ? "http://localhost:3000" : "http://10.0.2.2:3000";
+// const API_URL =
+//   Platform.OS === "ios" ? "http://localhost:3000" : "http://10.0.2.2:3000";
 
-// const API_URL = "http://18.191.17.41:3000"
+const API_URL = "http://18.191.17.41:3000"
 
 export default API_URL
